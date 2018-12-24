79cbd85d63f1eb18e856d685974355423c870657
lpru <lpru@lprus-iMac-23.local>
2018-12-24 11:41:46 +0700

    Initial commit

---
+++ app/src/main/java/com/yorch/hellogridview/MainActivity.java
@@ -0,0 +1,28 @@
+package com.yorch.hellogridview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GridView gridView = (GridView) findViewById(R.id.gridveiw);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this,"" ,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
